bb02a5c508606ce854367a07dcaf1d9a7328c8eb
Bra-Gabby-Marshal <sodeygabriel123@gmail.com>
2025-05-05 20:43:06 +0000

    created freelancers table

---
+++ database/migrations/2025_05_05_202622_create_freelancers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('freelancers', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('email')->unique();
+            $table->string('contact', 15);
+            $table->string('password');
+            $table->enum('gender', ['male', 'female', 'other'])->default('other');
+            $table->date('dob');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('verification_code')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('freelancers');
+    }
+};
