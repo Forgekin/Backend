354159633198384c6d7a77e3641a764f2350bc2b
Bra-Gabby-Marshal <sodeygabriel123@gmial.com>
2025-06-26 14:33:59 +0000

    added employer table, model and controller

---
+++ app/Models/Employer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Employer extends Authenticatable
+{
+    use HasFactory, Notifiable, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'name',
+        'company_name',
+        'contact',
+        'email',
+        'password',
+        'business_type',
+        'verification_status',
+        'email_verified_at'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
